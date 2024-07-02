076f240cfb23d4ff11ac5fbeeb9ce22d1138b0f8
github kang <etock19@naver.com>
2024-07-02 20:08:39 +0900

    #4 21-4. 인스턴스 변수

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -1,20 +1,18 @@
-﻿using System;
+﻿using using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CSPractice1
 {
     class FirstClass
     {
     }
-
     internal class Program
     {
         class ThirdClass
         {
-
         }
         static void Main(string[] args)
         {
@@ -27,11 +25,9 @@ namespace CSPractice1
              * 여러줄 주석
              *                         */
             /*** XML 주석 ***/
-
             // 출력
             Console.WriteLine("Hello World");
             Console.Write("뉴라인X");
-
             // #1 12-3. 기본 자료형 
             // 정수 
             Console.WriteLine(52 + 4 - 1);
@@ -40,11 +36,9 @@ namespace CSPractice1
 
             // 실수(Real Number)
             Console.WriteLine(52.0);
-
             // 문자(Character)
             Console.WriteLine('A');
             Console.WriteLine('가');
-
             // 문자열(String)
             Console.WriteLine("Hello World");
             // 이스케이프 문자
@@ -54,11 +48,9 @@ namespace CSPractice1
             // 문자열 인덱스 접근 [ ]
             Console.WriteLine("Hello World"[4]); // 'o'
             //Console.WriteLine("Hello World"[100]); // 예외발생
-
             // 주의
             Console.WriteLine('가' + '힣');
             // 문자열연결연산자X 덧셈 연산자로 인식
-
             // 논리(Boolean)
             Console.WriteLine(true);
             Console.WriteLine(false);
@@ -69,34 +61,27 @@ namespace CSPractice1
             Console.WriteLine(!true);
             Console.WriteLine(true && false);
             Console.WriteLine(true || false);
-
             // #1 12-4. 변수
             string name = "문자열 자료형";
-
             // #1 12-5. 복합대입연산자
             // += -= *= /= %=
             string output = "Hello"; ;
             output += " World";
             Console.WriteLine(output);
-
             // #1 12-6. 증감 연산자
             int number = 10;
             Console.WriteLine(number++); // 10
             Console.WriteLine(++number); // 12
             Console.WriteLine(number--); // 12
             Console.WriteLine(--number); // 10
-
             // #1 12-8 var 키워드
             var varnumber = 20;
             // var abc;  // x. 반드시 초기화 해줘야 함
-
             // #1 12-9 입력
             string input = Console.ReadLine();
             Console.WriteLine(input);
-
             // #1 12-10 자료형변환
             // 굉장히 중요
-
             // #2 13-1. if 조건문
             bool condition = true;
             if (condition)
@@ -111,7 +96,6 @@ namespace CSPractice1
             {
                 // other
             }
-
             // #2 13-2. switch 조건문
             int v = 0;
             switch (v)
@@ -123,16 +107,11 @@ namespace CSPractice1
                     // other
                     break;
             }
-
-
             // #2 13-6. 삼항연산자
             Console.WriteLine(number % 2 != 1 ? "짝수" : "홀수");
-
-
             // #3 14-1. 반복문과 배열
             int[] intArray = new int[100];
             Console.WriteLine(intArray[99]);
-
             // #3 14-2. while 반복문
             int i = 0;
             int[] intArray2 = { 52, 273, 32 };
@@ -141,14 +120,12 @@ namespace CSPractice1
                 Console.WriteLine(intArray2[i]);
                 i++; // 탈출을 위한 변수
             }
-
             // #3 14-6. foreach 반복문
             string[] foreachArray = { "사과", "배", "포도" };
             foreach (var item in foreachArray)
             {
                 Console.WriteLine(item);
             }
-
             // #4 21-2. 클래스 사용
             Random random = new Random();
             Console.WriteLine(random.Next(1, 7));
@@ -157,7 +134,6 @@ namespace CSPractice1
             Console.WriteLine(random.Next(1, 7));
             Console.WriteLine(random.Next(1, 7));
             Console.WriteLine(random.Next(1, 7));
-
             Console.WriteLine(Math.Abs(-9)); //9
             Console.WriteLine(Math.Ceiling(52.273)); //53
             Console.WriteLine(Math.Floor(52.273)); //52
@@ -167,6 +143,20 @@ namespace CSPractice1
             Console.WriteLine(Math.PI); //3.14159265358979
 
             // #4 21-3. 클래스 생성
+
+
+            // #4 21-4. 인스턴스 변수
+            User user1 = new User();
+            user1.name = "이미림";
+            user1.phoneNumber = "010-1234-5678";
+
+            Product p1 = new Product();
+            p1.name = "어른폰";
+            p1.price = 999999;
+
+            Product p2 = new Product() { price = 1000000, name = "아이폰" };
+            Product p3 = new Product() { price = 1000000 };
+
 
         }
     }
